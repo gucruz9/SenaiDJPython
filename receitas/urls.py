27761a9198a35de658aa8e283eeea011b1fa5f8f
gucruz9 <contato.one9@gmail.com>
2025-11-26 21:44:49 -0300

    aula01DJ_primeiroprojeto

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from.views import home, sobre, receita
+from.views import home
 
 
 urlpatterns = [
     path("", home),
-    path("sobre/", sobre),
-    path("receitas/", receita)
 ]
