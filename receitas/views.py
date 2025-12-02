2bf1cc9d73c3052e9d790d0c2bcf4c8e715ed37f
gucruz9 <contato.one9@gmail.com>
2025-12-01 21:46:20 -0300

    inacabado

--- receitas/views.py
+++ receitas/views.py
@@ -4,4 +4,7 @@ a = "Senai"
 def home(request):
     return render(request, "page/home.html", context={'nome' : a,})
 
+def receita(request):
+    return render(request, 'page/receita-view.html', context={'nome': a,})
+
 
